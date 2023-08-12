22451e85a4bd8d2607be918769421e9900cb2f4c
John Liu <stoic.llama@gmail.com>
2023-08-12 14:05:35 -0400

    Added dateFormattedNow() to healthcheck

--- src/api/routes/healthcheck.js
+++ src/api/routes/healthcheck.js
@@ -26,16 +26,40 @@ router.get("/", async (req, res) => {
             name: 'capstone-auth',
             message: 'OK',
             uptime: Math.floor(process.uptime()) + " seconds",
-            timestamp: new Date(Date.now()).toString() 
+            timestamp: formattedDateNow() // new Date(Date.now()).toString() 
         });
     } catch (error) {
         res.status(500).json({
             name: 'capstone-auth',
             message: error.message,
             uptime: Math.floor(process.uptime()) + " seconds",
-            timestamp: new Date(Date.now()).toString() 
+            timestamp: formattedDateNow() // new Date(Date.now()).toString() 
         });
     }
 });
+
+function formattedDateNow() {
+    var result = "" 
+    var d = new Date(Date.now())
+ 
+    // format ---> 'YYYY/MM/D hh:mm:ss SSS'
+ 
+    result = result + d.getFullYear()
+             + "/"
+             + (d.getMonth()+1)
+             + "/"
+             + d.getDate().toString().padStart(2,0) 
+             + " "
+             + d.getHours().toString().padStart(2,0)
+             + ":"
+             + d.getMinutes().toString().padStart(2,0)
+             + ":"
+             + d.getSeconds().toString().padStart(2,0)
+             + "."
+             + d.getMilliseconds().toString().padStart(3,0)
+ 
+    return result;
+ }
+
 
 export default router;
