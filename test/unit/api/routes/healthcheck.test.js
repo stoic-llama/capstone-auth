2e682409fdcc659ee9316562c0fbe9f67c2e2403
John Liu <stoic.llama@gmail.com>
2024-04-29 12:39:39 -0400

    enhanced healthcheck

--- test/unit/api/routes/healthcheck.test.js
+++ test/unit/api/routes/healthcheck.test.js
@@ -5,13 +5,15 @@ import app from "../../../../src/app" // "../../../src/app.js";
 
 
 describe("/api/v1/healthcheck", () => {
-  test("healthcheck responses successfully", async () => {
+  test("healthcheck responds successfully", async () => {
     const req = supertest(app);
     const res = await req
       .get("/api/v1/healthcheck")
 
     expect(res.status).toBe(200);
     expect(res.headers["content-type"]).toMatch(/json/);
+    expect(res.body).toHaveProperty("name");
+    expect(res.body).toHaveProperty("message");
   });
 });
 
